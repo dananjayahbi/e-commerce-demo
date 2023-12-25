62628b1204a9c64d6b3c6187fc076cab10a74add
djd <dananjayahbi@gmail.com>
2023-12-26 03:47:11 +0530

    Add AlertModal component for store deletion confirmation

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingsFormProps {
     initialData: Store
@@ -52,8 +53,29 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         }
     };
 
+    const onDelete = async () => {
+        try {
+            setLoading(true);
+            await axios.delete(`/api/stores/${params.storeId}`);
+            router.refresh();
+            router.push("/");
+            toast.success("Store deleted.");
+        } catch (error){
+            toast.error("Make sure you removed all products and categories first.");
+        } finally{
+            setLoading(false);
+            setOpen(false);
+        }
+    }
+
     return(
         <>
+            <AlertModal 
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
             <div className="flex items-center justify-between">
                 <Heading 
                     title="Settings"
