66bf1ee56f24a72974e3187ae0a67da37af0856d
djd <dananjayahbi@gmail.com>
2023-12-29 20:52:51 +0530

    Add billboard and navbar actions components

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,6 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container"
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("69334af2-785a-4dd7-90f1-f30c76edcf00");
+
     return ( 
-        <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+        </Container>
      );
 }
  
